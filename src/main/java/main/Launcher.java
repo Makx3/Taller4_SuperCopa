cbf5b2613eccfb72de38a0057fff006fd0b9b0c6
Makx3 <m.sepulveda51@ufromail.cl>
2023-11-30 10:54:20 -0300

    Commit launcher

---
+++ src/main/java/main/Launcher.java
@@ -0,0 +1,10 @@
+package main;
+
+import Guis.GuiTeams;
+
+public class Launcher {
+    public static void main(String[] args) {
+        GuiTeams guiTeams = new GuiTeams();
+        guiTeams.mostrarVentana();
+    }
+}
